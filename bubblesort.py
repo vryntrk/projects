dad50407cf6e653ab2be063168fcc1fa1468174d
Veli Resul Yentürk <vryntrk@gmail.com>
2025-01-31 23:12:41 +0300

    Update bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,14 @@
+def bubble_sort(array):
+    for _ in range(len(array)):
+        for j in range(0, len(array)-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+
+    return array
+
+
+number_list = [7, 3, 8, 2, 0, 4, 1, 6, 9, 5]
+print("Original List: ", number_list)
+
+sorted_list = bubble_sort(number_list)
+print("Sorted List: ", sorted_list)
